d5fc26780824b33f504e80dbe9c9758a7f5a7a33
Verminas <veremeeva96@gmail.com>
2024-01-15 12:35:51 +0300

    feat: add solution getIndexOf

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -269,8 +269,17 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let count = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      count = i;
+      break;
+    } else {
+      count = -1;
+    }
+  }
+  return count;
 }
 
 /**
