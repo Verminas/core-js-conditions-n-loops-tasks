2df6f2f2ea7d5ef26745dbf81b46847cf4db168e
Verminas <veremeeva96@gmail.com>
2024-02-09 18:37:17 +0300

    feat: add solution for sortByAsc, there is a problem with timeout

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -448,8 +448,18 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const array = arr;
+  for (let i = 0; i < arr.length - 1; i += 1) {
+    for (let j = 0; j < arr.length - 1 - i; j += 1) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        array[j] = arr[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
 }
 
 /**
